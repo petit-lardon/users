f8b63abfae4746b295c4b2a560517187c7395def
Julien LAVIE <jlavie@leadformance.com>
2018-07-22 23:50:39 +0200

    add service to know which button on menu is activated

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from "@angular/router";
+import {current} from "codelyzer/util/syntaxKind";
 
 @Component({
   selector: 'app-sidebar',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+    currentUrl: string;
 
-  constructor() { }
+  constructor(private router: Router) {
+      router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+  }
 
   ngOnInit() {
   }
